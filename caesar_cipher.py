42294658626a54b48b4ee5d0a37103d6f48bba64
Rafiul Hasan Rafi <67283985+rafidoth@users.noreply.github.com>
2021-07-24 19:20:17 +0600

    Add files via upload

---
+++ caesar_cipher.py
@@ -0,0 +1,21 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+
+def caesar(start_text, shift_amount, cipher_direction):
+    end_text =""
+    if direction == 'decode':   
+        shift_amount *= -1
+    for char in start_text:
+        if char in start_text:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+        end_text += alphabet[new_position]
+    print(f"The {cipher_direction}d text is {end_text}")    
+
+
+caesar(start_text= text, shift_amount=shift, cipher_direction= direction)
